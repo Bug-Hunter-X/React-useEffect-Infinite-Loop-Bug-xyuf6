369199ea2b396215536485e5e2f225eecd5f3819
BugHunter-X <askzenai@gmail.com>
2025-01-03 09:32:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,20 @@
+```javascript
+import React, { useState, useEffect } from 'react';
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This will cause an infinite loop because the count changes every render
+    setCount(count + 1);
+  }, []);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
